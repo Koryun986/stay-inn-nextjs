40e8410bb4e03233afa798a082331cf00be6d582
Koryun <79407889+Koryun986@users.noreply.github.com>
2024-02-04 14:47:27 +0400

    feat(logo component): add logo svg and whole logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./logo/Logo"
+
+export const Header = () => {
+  return (
+    <div className="flex items-center justify-between h-20">
+      <Logo /> 
+    </div>
+  ) 
+}
---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import LogoSvg from "./LogoSvg"
+
+export const Logo = () => {
+  return (
+    <Link className="flex items-end ttnc-logo text-current inline-block focus:outline-none focus:ring-0" href="/">
+      <LogoSvg />
+      <span className="ml-2 text-xl">StayInn</span> 
+    </Link>
+  )
+}
