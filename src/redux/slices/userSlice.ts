44459cd49e416f0a721e6447d66223426d91f5f6
Koryun <79407889+Koryun986@users.noreply.github.com>
2024-02-05 17:46:05 +0400

    feat(user slice): add not complite user slice

---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IUser {
+  name: string;
+  email: string;
+  avatar_url?: string;
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    name: "Anonymous User",
+    email: "user@anonymous.com",
+  },
+  reducers: {},
+});
+
+export default userSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currencyReducer from "./slices/currencySlice";
 import languageReducer from "./slices/languageSlice";
+import userReducer from "./slices/userSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       currency: currencyReducer,
       language: languageReducer,
+      user: userReducer,
     },
   });
 };
